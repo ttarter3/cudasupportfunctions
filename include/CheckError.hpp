fe3e5f15e5e251c36a0f1638307f64ee44d5a130
ttarter3 <ttarter3@gmail.com>
2024-01-28 20:13:44 -0600

    refactor naming convention to meet google style guide

---
+++ include/CheckError.hpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+static const char *_getErrorEnum(cudaError_t error) {
+		return cudaGetErrorName(error);
+}
+
+template <typename T>
+void Check(T result, char const * const func, const char * const file, int const line) {
+  if (result) {
+    fprintf(stderr, "Error at %s:%d code=%d(%s) \"%s\" \n", file, line, static_cast<unsigned int>(result), _getErrorEnum(result), func);
+    exit(EXIT_FAILURE);
+  }
+}
+
+#define CheckErrors(val) Check((val), #val, __FILE__, __LINE__)
+
+void CheckKernelError() {
+
+  // always check for synchronous errors from kernel launch
+  CheckErrors(cudaGetLastError());
+  
+  // only wat to do cuda device synchronize when we're debugging DISABLE before release
+  #ifndef NDEBUG
+    CheckErrors(cudaDeviceSynchronize());
+  #endif
+
+}
